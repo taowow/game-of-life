b89214447dd459b25a70bb584552016139387a31
Daniel Larson <taowow@gmail.com>
2014-09-10 17:34:25 -0700

    Back to goodness 4 - Test

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -3,6 +3,7 @@ package com.wakaleo.gameoflife.domain;
 public enum Cell {
     LIVE_CELL("*"), DEAD_CELL(".");
 
+    private String foo;
     private String symbol;
 
     private Cell(final String initialSymbol) {
